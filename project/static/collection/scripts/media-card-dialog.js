06ef445ae26a3a1f3e7d2a8da28f10c810c3826a
DominikG14 <dgasiorek14@gmail.com>
2025-03-28 16:48:09 +0100

    left-right buttons

--- project/static/collection/scripts/media-card-dialog.js
+++ project/static/collection/scripts/media-card-dialog.js
@@ -1,18 +1,23 @@
+let currentMediaNum = 0;
+
+const lastMedia = document.querySelector('.media-cards .media-card:last-child');
+const maxMediaNum = Number(lastMedia.dataset.mediaNum);
+
 document.addEventListener('click', event => {
-  if(event.target.hasAttribute("data-open-media-card-dialog")){
+  if(event.target.hasAttribute('data-open-media-card-dialog')){
     const dialog = document.querySelector('#media-card-dialog');
     const image = document.querySelector('#media-display__image');
 
     image.src = event.target.src;
+    currentMediaNum = Number(event.target.dataset.mediaNum);
     dialog.style.display = 'block';
   }
 });
 
 
 document.addEventListener('click', event => {
-  if(event.target.hasAttribute("data-close-media-card-dialog")){
+  if(event.target.hasAttribute('data-close-media-card-dialog')){
     const dialog = document.querySelector('#media-card-dialog');
-    console.log('Works?');
     dialog.style.display = 'none';
   }
 });
@@ -22,5 +27,26 @@ document.addEventListener('keydown', event => {
   if(event.key === 'Escape'){
     const dialog = document.querySelector('#media-card-dialog');
     dialog.style.display = 'none';
+  }
+});
+
+
+document.addEventListener('click', event => {
+  if(event.target.hasAttribute('data-change-media-right')){
+    if(currentMediaNum === maxMediaNum) return;
+    currentMediaNum++;
+    const newImage = document.querySelector(`[data-media-num="${currentMediaNum}"]`);
+    const currentImage = document.querySelector('#media-display__image');
+    currentImage.src = newImage.src;
+  }
+});
+
+document.addEventListener('click', event => {
+  if(event.target.hasAttribute('data-change-media-left')){
+    if(currentMediaNum === 1) return;
+    currentMediaNum--;
+    const newImage = document.querySelector(`[data-media-num="${currentMediaNum}"]`);
+    const currentImage = document.querySelector('#media-display__image');
+    currentImage.src = newImage.src;
   }
 });
