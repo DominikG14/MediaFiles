0da7dac70935171b920148448cc030804ab49e29
DominikG14 <dgasiorek14@gmail.com>
2025-04-01 18:35:43 +0200

    creatd django tag for media-card component

---
+++ project/apps/collection/templatetags/collection_components.py
@@ -0,0 +1,13 @@
+from django import template
+
+register = template.Library()
+
+
+@register.inclusion_tag('collection/components/media-card.html')
+def media_card(src='', alt='', num=None, class_list=None):
+    return {
+        'src': src,
+        'alt': alt,
+        'num': num,
+        'class_list': class_list or []
+    }
